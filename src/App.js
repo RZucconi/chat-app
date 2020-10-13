7d285bf67c2cee70cec58881d5307403a5ebc463
RZucconi <romain.zucconi@gmail.com>
2020-10-13 20:58:53 +0200

    delete unused import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import Contact from './components/Contact';
 import ContactList from './components/ContactList';
 
 import './App.css';
-import { findAllByDisplayValue } from '@testing-library/react';
-
 
 function App() {
   return (

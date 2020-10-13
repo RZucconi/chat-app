eeb1b1f7d84ba930c5fc9dc4541653451c0c1ddd
RZucconi <romain.zucconi@gmail.com>
2020-10-13 19:15:34 +0200

    complete react 3 challenge

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React from 'react';
 
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 import './App.css';
 import { findAllByDisplayValue } from '@testing-library/react';
 
-const user = [
-  {
-    name: 'Ava Duncan',
-    avatar: 
-    "https://randomuser.me/api/portraits/women/19.jpg",
-    online: true
-  },
-  {
-    name: 'Cath Myers',
-    avatar: 
-    "https://randomuser.me/api/portraits/women/89.jpg",
-    online: false
-  },
-  {
-    name: 'Dean Richard',
-    avatar: 
-    "https://randomuser.me/api/portraits/men/76.jpg",
-    online: true
-  }
-]
 
 function App() {
   return (
     <div>
-      <Contact {...user[0]}/>
-      <Contact {...user[1]}/>
-      <Contact {...user[2]}/>
+      <ContactList />
     </div>
   );
 }
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import './contact.css';
+
+const users = [
+  {
+    name: 'Robert Reyes',
+    avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+    online: false
+  },
+  {
+    name: 'Nellie Caldwell',
+    avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+    online: true
+  },
+  {
+    name: 'Vernon Mason',
+    avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+    online: true
+  },
+  {
+    name: 'Erica Hunt',
+    avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+    online: false
+  },
+  {
+    name: 'Juanita Phillips',
+    avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+    online: true
+  }
+];
+
+const ContactList = () => (
+    <div>
+        { users.map(user => (
+            <div className="Contact">
+            <img className="avatar" src={user.avatar} alt={user.name}
+            />
+            <div>
+                <h2 className="name">{user.name}</h2>
+                <div className="status">
+                    <span className={user.online ? 'status-online' : 'status-offline'}></span>
+                    <div className="status-text">{user.online ? "Online" : "Offline"}</div>    
+                </div>
+            </div>
+        </div>
+        ))}
+    </div>
+)
+
+export default ContactList;

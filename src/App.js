c9462baee9d769fc286dfc0e2bd318ed91aae744
RZucconi <romain.zucconi@gmail.com>
2020-10-13 11:30:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Contact from './components/Contat';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
+}
+
+export default App;

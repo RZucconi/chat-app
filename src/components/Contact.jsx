459e32c04a02c3a40591c29bbed4b4a6abb4cfdb
RZucconi <romain.zucconi@gmail.com>
2020-10-14 12:50:06 +0200

    completed react quest 5 challenge

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { ReactComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import './contact.css';
+import { render } from '@testing-library/react';
 
-function Contact({ name, avatar, online }){
-    return(
-        <div className="Contact">
-            <img className="avatar" src={avatar} alt={name}
-            />
-            <div>
-                <h2 className="name">{name}</h2>
-                <div className="status">
-                    <span className={online ? 'status-online' : 'status-offline'}></span>
-                    <div className="status-text">{online ? "Online" : "Offline"}</div>    
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={online:this.props.online};
+    }
+
+    render(){
+        return(
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name}
+                />
+                <div>
+                    <h2 className="name">{this.props.name}</h2>
+                    <div className="status" onClick={event => {this.setState({online: !this.state.online});}}>
+                        <span className={this.state.online ? 'status-online' : 'status-offline'}></span>
+                        <div className="status-text">{this.state.online ? "Online" : "Offline"}</div>    
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 Contact.propTypes = {
